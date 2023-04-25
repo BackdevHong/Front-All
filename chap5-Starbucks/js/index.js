90a6c06f17324cc8b3f4cdaade0dd490b29df41f
backdevhong <ghddls0704@kakao.com>
2023-04-25 20:19:26 +0900

    add : Delay animation

--- chap5-Starbucks/js/index.js
+++ chap5-Starbucks/js/index.js
@@ -1,17 +1,45 @@
 const searchEl = document.querySelector(".search");
 const searchInputEl = searchEl.querySelector("input");
 
-searchEl.addEventListener('click', () => {
+searchEl.addEventListener("click", () => {
   searchInputEl.focus();
-})
+});
 
-searchInputEl.addEventListener('focus', () => {
+searchInputEl.addEventListener("focus", () => {
   searchEl.classList.add("focused");
-  searchInputEl.setAttribute("placeholder", "통합검색")
-})
+  searchInputEl.setAttribute("placeholder", "통합검색");
+});
 
-searchInputEl.addEventListener('blur', () => {
+searchInputEl.addEventListener("blur", () => {
   searchEl.classList.remove("focused");
-  searchInputEl.setAttribute("placeholder", "")
-})
+  searchInputEl.setAttribute("placeholder", "");
+});
 
+const badgeEl = document.querySelector("header .badges");
+
+window.addEventListener(
+  "scroll",
+  _.throttle(() => {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 300)
+);
+
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+
+fadeEls.forEach((fadeEl, idx) => {
+  gsap.to(fadeEl, 1, {
+    delay: (idx + 1) * 0.7,
+    opacity: 1,
+  });
+});
