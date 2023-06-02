15fab45c99d9ab82deda7421cea00920b99f1a27
backdevhong <ghddls0704@kakao.com>
2023-06-02 18:52:16 +0900

    hero start

--- chap6-Apple-ipad/js/main.js
+++ chap6-Apple-ipad/js/main.js
@@ -29,10 +29,13 @@ function hideBasket() {
 
 // Search(검색)
 const headerEl = document.querySelector("header");
+const headerMenuEls = [...headerEl.querySelectorAll("ul.menu > li")];
 const searchWrapEl = headerEl.querySelector(".search-wrap");
 const searchStarterEl = headerEl.querySelector(".search-starter");
 const searchCloserEl = searchWrapEl.querySelector(".search-closer");
 const searchShadowEl = searchWrapEl.querySelector(".shadow");
+const searchInputEl = searchWrapEl.querySelector("input");
+const searchDelayEls = [...searchWrapEl.querySelectorAll("li")];
 
 searchStarterEl.addEventListener("click", () => {
   showSearch();
@@ -49,9 +52,26 @@ searchShadowEl.addEventListener("click", () => {
 function showSearch() {
   headerEl.classList.add("searching");
   document.documentElement.classList.add("fixed");
+  headerMenuEls.reverse().forEach((el, idx) => {
+    el.style.transitionDelay = (idx * 0.4) / headerMenuEls.length + "s";
+  });
+  searchDelayEls.forEach((el, idx) => {
+    el.style.transitionDelay = (idx * 0.4) / searchDelayEls.length + "s";
+  });
+  setTimeout(() => {
+    searchInputEl.focus();
+  }, 600);
 }
 
 function hideSearch() {
   headerEl.classList.remove("searching");
   document.documentElement.classList.remove("fixed");
+  headerMenuEls.reverse().forEach((el, idx) => {
+    el.style.transitionDelay = (idx * 0.4) / headerMenuEls.length + "s";
+  });
+  searchDelayEls.reverse().forEach((el, idx) => {
+    el.style.transitionDelay = (idx * 0.4) / searchDelayEls.length + "s";
+  });
+  searchDelayEls.reverse();
+  searchInputEl.value = "";
 }
