c0f0634f24f4b7d2f6bf592af195ff132c62738f
backdevhong <ghddls0704@kakao.com>
2023-07-27 19:39:13 +0900

    commit

--- chap8-disney-plus-app/src/component/Banner.js
+++ chap8-disney-plus-app/src/component/Banner.js
@@ -42,7 +42,7 @@ const Banner = () => {
             <Iframe
               src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
               width="640"
-              height="350"
+              height="360"
               allow="autoplay; fullscreen"
             ></Iframe>
           </HomeContainer>
