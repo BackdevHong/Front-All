8829f5a8d30f95f63396eae0f4c632b205bc4989
backdevhong <ghddls0704@kakao.com>
2023-07-27 19:38:35 +0900

    commit

--- chap8-disney-plus-app/src/component/Row.js
+++ chap8-disney-plus-app/src/component/Row.js
@@ -3,9 +3,12 @@ import { useEffect } from "react";
 import { useState } from "react";
 import axios from "../api/axios";
 import "./styles/Row.css";
+import MovieModal from "./MovieModal";
 
 const Row = ({ title, id, fetchUrl }) => {
   const [movies, setMovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelection] = useState({});
 
   const fetchMovieData = useCallback(async () => {
     const response = await axios.get(fetchUrl);
@@ -16,12 +19,24 @@ const Row = ({ title, id, fetchUrl }) => {
     fetchMovieData();
   }, [fetchMovieData]);
 
+  const handleClick = (movie) => {
+    setModalOpen(true);
+    setMovieSelection(movie);
+  };
+
   return (
     <div>
       <h2>{title}</h2>
       <div className="slider">
         <div className="slider__arrow-left">
-          <span className="arrow">{"<"}</span>
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+            }}
+          >
+            {"<"}
+          </span>
         </div>
         <div className="row__posters" id={id}>
           {movies.map((movie) => (
@@ -30,6 +45,9 @@ const Row = ({ title, id, fetchUrl }) => {
               className="row__poster"
               src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
               alt={movie.name}
+              onClick={() => {
+                handleClick(movie);
+              }}
             />
           ))}
         </div>
@@ -44,6 +62,10 @@ const Row = ({ title, id, fetchUrl }) => {
           </span>
         </div>
       </div>
+
+      {modalOpen && (
+        <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
+      )}
     </div>
   );
 };
