ea9e8deb0a21bfcd87c49efa1c70d4c86bf35ec9
backdevhong <ghddls0704@kakao.com>
2023-09-01 22:22:50 +0900

    context

--- chap9-react-context-app/client/src/components/Type.js
+++ chap9-react-context-app/client/src/components/Type.js
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import Products from "./Products";
 import Options from "./Options";
 import ErrorBanner from "./ErrorBanner";
+import { OrderContext } from "../context/OrderContext";
 
 const Type = ({ orderType }) => {
   const [items, setItems] = useState([]);
   const [error, setError] = useState({});
+  const [orderData, updateItemCount] = useContext(OrderContext);
 
   useEffect(() => {
     loadItems(orderType);
@@ -24,6 +26,8 @@ const Type = ({ orderType }) => {
     }
   };
 
+  console.log(orderData);
+
   const ItemComponent = orderType === "products" ? Products : Options;
 
   const optionItems = items.map((item) => (
@@ -31,6 +35,9 @@ const Type = ({ orderType }) => {
       key={item.name}
       name={item.name}
       imagePath={item.imagePath}
+      updateItemCount={(itemName, newItemCount) => {
+        updateItemCount(itemName, newItemCount, orderType);
+      }}
     />
   ));
 
@@ -42,7 +49,7 @@ const Type = ({ orderType }) => {
     <div>
       <h2>주문 종류</h2>
       <p>하나의 가격</p>
-      <p>총 가격 :</p>
+      <p>총 가격 : {orderData.totals[orderType]}</p>
       <div
         style={{
           display: "flex",
