d7976da5a29425193ce9e56888f6b2d8b351dd14
backdevhong <ghddls0704@kakao.com>
2023-12-04 16:14:28 +0900

    add : TodoItemCreator

---
+++ ChapFolder-React/chap14-react-recoil-todo-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TodoItemCreator from './components/TodoItemCreator';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoItemCreator></TodoItemCreator>
+    </div>
+  );
+}
+
+export default App;
